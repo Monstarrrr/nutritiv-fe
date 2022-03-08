7d3f874aac437c9349f27e9309662364c831788f
Monstar <hugo.bonpain@gmail.com>
2022-03-08 21:43:48 +0100

    Began implementing useReducer

--- src/Components/ProfilePassword.js
+++ src/Components/ProfilePassword.js
@@ -1,12 +1,40 @@
 import React, { useState } from 'react'
-import nutritivApi, { apiUpdatePassword } from '../Api/nutritivApi'
+import { useReducer } from 'react'
+import nutritivApi from '../Api/nutritivApi'
+
+const reducer = (prevState, action) => {
+  switch(action.type) {
+    case 'ADD_PASSWORD':
+      return {
+        ...prevState,
+        passwordInput: {
+          ...prevState.passwordInput,
+          [action.payload.target.name]: action.payload.target.value
+        }
+      }
+    default:
+      throw new Error();
+  }
+}
 
 export const ProfilePassword = () => {
-  const [passwordInput, setPasswordInput] = useState({
-    oldPass: "",
-    newPass: "",
-    confirmNewPass: ""
+  const [
+    { 
+      passwordInput,
+    }, 
+    dispatch
+  ] = useReducer(reducer, {
+    passwordInput: {
+      oldPass: "",
+      newPass: "",
+      confirmNewPass: ""
+    }
   })
+  // const [passwordInput, setPasswordInput] = useState({
+  //   oldPass: "",
+  //   newPass: "",
+  //   confirmNewPass: ""
+  // })
   const [updatePasswordResponse, setUpdatePasswordResponse] = useState(null)
   const [loadingUpdatePassword, setLoadingUpdatePassword] = useState(false)
   
@@ -14,16 +42,22 @@ export const ProfilePassword = () => {
     isEmpty: false,
     isNotMatching: false,
   })
+
+  console.log('# passwordInput :', passwordInput)
+  console.log('# errorPasswordInput :', errorPasswordInput)
+  console.log('# !passwordInput.oldPass :', !passwordInput.oldPass)
   
   const passwordInputsValidation = () => {
     setUpdatePasswordResponse(null)
     let passwordEmpty = false;
     let passwordNotMatching = false;
+    
     if(
       !passwordInput.oldPass ||
       !passwordInput.newPass ||
       !passwordInput.confirmNewPass
     ) {
+      console.log("IF ENTERED SUCCESSFULLY...")
       setErrorPasswordInput({
         ...errorPasswordInput,
         isEmpty: true,
@@ -35,6 +69,7 @@ export const ProfilePassword = () => {
         isEmpty: false,
       })
     }
+    
     if(passwordInput.newPass !== passwordInput.confirmNewPass) {
       setErrorPasswordInput({
         ...errorPasswordInput,
@@ -51,12 +86,12 @@ export const ProfilePassword = () => {
   }
   
   // HANDLERS
-  const handlePasswordInputChange = (e) => {
-    setPasswordInput({
-      ...passwordInput,
-      [e.target.name]: e.target.value
-    })
-  }
+  // const handlePasswordInputChange = (e) => {
+  //   setPasswordInput({
+  //     ...passwordInput,
+  //     [e.target.name]: e.target.value
+  //   })
+  // }
   
   const handleSubmitUpdatePassword = async (e) => {
     e.preventDefault()
@@ -64,6 +99,7 @@ export const ProfilePassword = () => {
     const isValid = passwordInputsValidation();
     
     if(isValid) {
+      console.log("INPUTS VALID, API CALL STARTING...")
       setLoadingUpdatePassword(true)
       setErrorPasswordInput(
         Object.keys(errorPasswordInput).forEach(v => (
@@ -77,11 +113,15 @@ export const ProfilePassword = () => {
           passwordInput
         )
         setUpdatePasswordResponse(data)
-        setPasswordInput({
-          oldPass: "",
-          newPass: "",
-          confirmNewPass: ""
+        dispatch({
+          type: "ADD_PASSWORD",
+          payload: ""
         })
+        // setPasswordInput({
+        //   oldPass: "",
+        //   newPass: "",
+        //   confirmNewPass: ""
+        // })
       } catch (err) {
         setUpdatePasswordResponse({})
         console.log('# /users/reset_password :', err)
@@ -99,7 +139,9 @@ export const ProfilePassword = () => {
           </h3>
           <br />
           <input
-            onChange={handlePasswordInputChange}
+            onChange={event =>
+              dispatch({ type: "ADD_PASSWORD", payload: event })
+            }
             name='oldPass'
             placeholder='Current password...'
             type="password"
@@ -107,7 +149,9 @@ export const ProfilePassword = () => {
           />
           <br />
           <input
-            onChange={handlePasswordInputChange}
+            onChange={event =>
+              dispatch({ type: "ADD_PASSWORD", payload: event.target.value})
+            }
             name='newPass'
             placeholder='New password...'
             type="password"
@@ -115,7 +159,9 @@ export const ProfilePassword = () => {
           />
           <br />
           <input
-            onChange={handlePasswordInputChange}
+            onChange={event =>
+              dispatch({ type: "ADD_PASSWORD", payload: event.target.value})
+            }
             name='confirmNewPass'
             placeholder='Confirm new password...'
             type="password"
@@ -128,7 +174,7 @@ export const ProfilePassword = () => {
           />
           <br />
           { 
-            errorUpdatePasswordNotMatching && (
+            errorPasswordInput.isNotMatching && (
               <span style={{color: "red"}}>
                 The password do not match the confirmation field.
               </span>
@@ -136,7 +182,7 @@ export const ProfilePassword = () => {
           }
           <br />
           { 
-            errorUpdatePasswordEmpty && (
+            errorPasswordInput.isEmpty && (
               <span style={{color: "red"}}>
                 Please fill in all the fields.
               </span>
