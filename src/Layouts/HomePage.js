a36001ed6c535a6598b0620b924d5c372609e39e
Monstar <hugo.bonpain@gmail.com>
2022-03-11 21:12:53 +0100

    [WIP] 429 Error handler

--- src/Layouts/HomePage.js
+++ src/Layouts/HomePage.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import nutritivApi from '../Api/nutritivApi';
 
 export default function HomePage() {
   console.log("##### HomePage render #####");
   
   const loggedIn = useSelector(state => state.user.loggedIn)
   
+  const handleSpam = async () => {
+    for(let i=0; i<60; i = i+1){
+      try {
+        const { data } = await nutritivApi.get(
+          `/users/self`,
+        )
+        console.log('# data :', data)
+      } catch(err) {
+        console.error('/users/selfAvatar:', err)
+      }
+    }
+  }
+
   return (
     <div>
       <h1>Homepage</h1>
@@ -16,6 +30,9 @@ export default function HomePage() {
           </div>
         )
       }
+      <button onClick={handleSpam}>
+        Spam :)
+      </button>
     </div>
   );
 }
