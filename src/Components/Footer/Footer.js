729ee84a914f586870e3856fb135b739a1c5fa79
Monstar <hugo.bonpain@gmail.com>
2022-10-29 18:17:55 +0200

    [WIP] Responsive footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,9 +3,9 @@ import { css } from '@emotion/react'
 import styled from '@emotion/styled'
 import React, { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import { tokens } from '../../Helpers/styleTokens'
+import { mediaQueries, tokens } from '../../Helpers/styleTokens'
 import { LogoLink, navLinksItems } from '../Header/Navbar'
-import { NutriButton } from '../NutriButton'
+import { Icon } from '../Icons/Icon'
 
 const NavLink = styled(Link)`
   color: ${props => props.active ? tokens.color.secondary : tokens.color.contrastLight};
@@ -25,79 +25,116 @@ export const Footer = () => {
         user-select: none;
       `}
     >
-      <div
-        css={css`
-          display: flex;
-          justify-content: space-between;
-          margin: 50px 0;
-        `}
-      >
-        <LogoLink style={{ minWidth: "180px" }}>
-          <img 
-            alt="nutritiv logo"
-            src="/logo.png"
-          />
-        </LogoLink>
-        <ul
+      <div css={css`margin: ${tokens.spacing.xxl};`}>
+        <div
           css={css`
-            display: flex;
-            list-style-type: none;
-            padding: 0;
-          `}
-        >
-          {navLinksItems.map(item => (
-            <NavLink
-              active={location.pathname === item.link ? 1 : undefined}
-              key={item.link}
-              onClick={() => setActive(item.link)}
-              css={css`
-                margin: 0 ${tokens.spacing.lg};
-                font-weight: ${tokens.font.fontWeight.medium};
-                letter-spacing: 1px;
-                text-decoration: none;
-                text-transform: uppercase;
-              `}
-              to={item.link}
-            >
-              {item.label}
-            </NavLink>
-          ))}
-        </ul>
-        <div 
-          css={css`
+            align-items: center;
             display: flex;
             justify-content: space-between;
-            min-width: 180px;
+            ${mediaQueries({
+              flexDirection: ["column", "column", "row", "row"],
+            })}
           `}
         >
-          <NutriButton label="test"/>
+          <LogoLink 
+            style={{ 
+              display: "flex",
+              minWidth: "180px", 
+              justifyContent: "center",
+            }}
+          >
+            <img 
+              alt="nutritiv logo"
+              src="/logo.png"
+            />
+          </LogoLink>
+          <ul
+            css={css`
+              display: flex;
+              list-style-type: none;
+              padding: 0;
+              margin: ${tokens.spacing.xxl} auto;
+            `}
+          >
+            {navLinksItems.map(item => (
+              <NavLink
+                active={location.pathname === item.link ? 1 : undefined}
+                key={item.link}
+                onClick={() => setActive(item.link)}
+                css={css`
+                  margin: 0 ${tokens.spacing.lg};
+                  font-weight: ${tokens.font.fontWeight.medium};
+                  letter-spacing: 1px;
+                  text-decoration: none;
+                  text-transform: uppercase;
+                `}
+                to={item.link}
+              >
+                {item.label}
+              </NavLink>
+            ))}
+          </ul>
+          <div 
+            css={css`
+              display: flex;
+              justify-content: center;
+              min-width: 180px;
+              > svg {
+                margin: 0 ${tokens.spacing.sm};
+              }
+            `}
+          >
+            <Icon
+              name="github" 
+              color={tokens.color.contrastLight}
+              filled
+              height={"28px"}
+              width={"28px"}
+            />
+            <Icon
+              name="star" 
+              color={tokens.color.contrastLight}
+              filled
+              height={"28px"}
+              width={"28px"}
+            />
+            <Icon
+              name="beaker" 
+              color={tokens.color.contrastLight}
+              filled
+              height={"28px"}
+              width={"28px"}
+            />
+          </div>
         </div>
-      </div>
-      <div 
-        css={css`
-          font-size: ${tokens.font.fontSize.sm};
-          margin-bottom: ${tokens.spacing.sm};
-        `}
-      >
-        2022 by Kiwi Labs, All Rights Reserved. <a>Hire us</a>.
-      </div>
-      <div
-        css={css`
-          font-size: ${tokens.font.fontSize.xs};
-          margin-bottom: ${tokens.spacing.lg};
-          & > a {
-            color: ${tokens.color.accentStrong};
-            text-decoration: none;
-          }
-        `}
-      >
-        This site is protected by reCAPTCHA and the Google&nbsp;
-        <a href="https://policies.google.com/privacy">Privacy Policy</a> and&nbsp;
-        <a href="https://policies.google.com/terms">Terms of Service</a> apply.&nbsp;
-        <Link to="/releases">
-          Releases
-        </Link>
-        .
+        
+        <div 
+          css={css`
+            font-size: ${tokens.font.fontSize.sm};
+            margin-bottom: ${tokens.spacing.sm};
+            margin-top: ${tokens.spacing.xxl};
+          `}
+        >
+          2022 by Kiwi Labs, All Rights Reserved. <a>Hire us</a>.
+        </div>
+        <div
+          css={css`
+            font-size: ${tokens.font.fontSize.xs};
+            margin-bottom: ${tokens.spacing.lg};
+            & > a {
+              color: ${tokens.color.accentStrong};
+              text-decoration: none;
+            }
+          `}
+        >
+          This site is protected by reCAPTCHA and the Google&nbsp;
+          <a href="https://policies.google.com/privacy">Privacy Policy</a> and&nbsp;
+          <a href="https://policies.google.com/terms">Terms of Service</a> apply.&nbsp;
+          <Link to="/releases">
+            Releases
+          </Link>
+          .
+        </div>
       </div>
     </footer>
   )
