ddb44cc4dd15d5ff4197a5321ebbf618054869af
Monstar <hugo.bonpain@gmail.com>
2022-10-29 17:35:24 +0200

    Revert "Revert 'Fix conflicts'"
    This reverts commit 883757d644f3095a769b285de2090804a3b13427.

--- src/App.js
+++ src/App.js
@@ -292,7 +292,7 @@ function App() {
           />
           <NavbarMenu open={mobileNavMenu} />
             <Suspense fallback={null}>
-              <AnimatePresence exitBeforeEnter>
+              {/* <AnimatePresence exitBeforeEnter> */}
                 <Routes
                   location={location} 
                   key={location.pathname}
@@ -370,7 +370,7 @@ function App() {
                     </Route>
                   </Route>
                 </Routes>
-              </AnimatePresence>
+              {/* </AnimatePresence> */}
               <Canvas
                 onCreated={(state) =>
                   state.events.connect(canvasWrapperRef.current)
